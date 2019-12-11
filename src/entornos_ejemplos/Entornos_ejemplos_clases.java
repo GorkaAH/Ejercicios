eb859ec0e97521f9d15a482df05a35232fcaa7a6
dam <dam@172.26.39.50>
2019-12-11 12:15:05 +0100

    Verion1

---
+++ src/entornos_ejemplos/Entornos_ejemplos_clases.java
@@ -0,0 +1,25 @@
+package entornos_ejemplos;
+
+public class Entornos_ejemplos_clases {
+
+    public static void main(String[] args) {
+        /*Clase1 objeto;
+        Clase1 objeto2;
+        String nombre ="Hola a todos";
+        int edad = 22;
+        objeto = new Clase1();
+        objeto2 = new Clase1("pepito",15);
+        System.out.println(objeto);
+       }*/
+
+        Clase1 n;
+        int z = 3;
+        int y = 7;
+        n = new Clase1();
+        int x = n.metodo(z, y);
+        
+    }
+    
+    }
+
+
